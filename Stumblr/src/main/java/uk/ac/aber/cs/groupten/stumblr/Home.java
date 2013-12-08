55f486aaf3e3a53e3599a09dc820cc6b7d20b75f
Charlie Newey <charlie@assemblyco.de>
2013-12-08 14:40:56 +0000

    Updated main activity and interface

--- Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/Home.java
+++ Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/Home.java
@@ -4,14 +4,14 @@ import android.os.Bundle;
 
 import uk.ac.aber.cs.groupten.stumblr.data.Route;
 
-public abstract class Home extends AbstractActivity {
+public class Home extends AbstractActivity {
     /**
      * Loads the activity on creation (using a bundle if one is present)
      * @param savedInstanceState The bundle containing the saved instance state.
      */
     public void stumblrOnCreate(Bundle savedInstanceState) {
         // Called by super().onCreate
-        setContentView(R.layout.activity_abstract);
+        setContentView(R.layout.activity_home);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -23,5 +23,5 @@ public abstract class Home extends AbstractActivity {
     /**
      * Begin the Route entry activity
      */
-    public abstract void startCreateRouteIntent(Route r);
+    public void startCreateRouteIntent(Route r) {};
 }
