5d3ff45fdb409614884fb64f4585dcff4e650f6c
Daniel <dac46@aber.ac.uk>
2013-12-13 17:40:31 +0000

    Merged Camera and Post_Request together

--- Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/Home.java
+++ Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/Home.java
@@ -3,12 +3,22 @@ package uk.ac.aber.cs.groupten.stumblr;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import uk.ac.aber.cs.groupten.stumblr.data.Route;
 
@@ -25,6 +35,62 @@ public class Home extends AbstractActivity {
             // do stuff
         }
     }
+
+
+
+    private class NetworkTask extends AsyncTask<String, Void, HttpResponse> {
+        @Override
+        protected HttpResponse doInBackground(String... params) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://www.parityb.it");
+            try {
+                JSONObject data = getData();
+                StringEntity stringData = new StringEntity(data.toString(4));
+
+                httppost.setEntity(stringData);
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                return response;
+            } catch (Exception e) {
+                Log.e("ERROR", e.toString());
+                return null;
+            }
+        }
+    }
+
+
+    /**
+     * Posts the data to the server
+     */
+    public void postData(View view) {
+        new NetworkTask().execute();
+    }
+
+    private JSONObject getData() {
+        //Data to be sent:
+        JSONObject data = new JSONObject();
+        try {
+            JSONObject walk = new JSONObject();
+            walk.put("walkTitle", "Road Runner");
+            walk.put("shortDescription", "meep meep!");
+            walk.put("longDescription", "meep meep meep meep!");
+            walk.put("walkHours", 42.0);
+            walk.put("walkDistance", 42.0);
+            JSONArray locations = new JSONArray();
+            locations.put(1);
+            locations.put(2);
+            locations.put(3);
+            locations.put(4);
+            walk.put("locations", locations);
+            data.put("walk", walk);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+
 
     /**
      * Begin the Route entry activity
