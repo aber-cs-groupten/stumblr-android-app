2d3d2e3e99e570dd0506d1066b0f8288734829c1
Charles Newey <charlie@assemblyco.de>
2013-12-27 01:02:55 +0000

    Updated layout and button actions

--- Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/Home.java
+++ Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/Home.java
@@ -1,14 +1,8 @@
 package uk.ac.aber.cs.groupten.stumblr;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import uk.ac.aber.cs.groupten.stumblr.data.Route;
 
@@ -20,7 +14,6 @@ public class Home extends AbstractActivity {
     public void stumblrOnCreate(Bundle savedInstanceState) {
         // Called by super().onCreate
         setContentView(R.layout.activity_home);
-
         if (savedInstanceState == null) {
             // do stuff
         }
@@ -30,27 +23,6 @@ public class Home extends AbstractActivity {
      * Begin the Route entry activity
      */
     public void startCreateRouteIntent(View v) {
-        startActivity(new Intent(getApplicationContext(), CreateRoute.class));
-    }
-
-    /**
-     * Start Camera activity
-     */
-    public void startCamera(View v) {
-        startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), 0);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 0 && resultCode == RESULT_OK) {
-            ImageView mImageView = new ImageView(getApplicationContext());
-            Bundle extras = data.getExtras();
-            Bitmap b = (Bitmap) extras.get("data");
-            if (b != null) {
-                mImageView.setImageBitmap(b);
-            }
-        }
+        startActivity(new Intent(getApplicationContext(), CreateWaypoint.class));
     }
 }
