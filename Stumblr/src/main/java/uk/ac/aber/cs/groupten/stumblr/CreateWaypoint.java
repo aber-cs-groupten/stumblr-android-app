445899fcb1c35ec112794e955189adf0597e8d22
Charles Newey <charlie@assemblyco.de>
2013-12-27 01:03:26 +0000

    Changed layout fetching and photo acquisition

--- Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/CreateWaypoint.java
+++ Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/CreateWaypoint.java
@@ -6,31 +6,33 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 
 import java.util.List;
 
 import uk.ac.aber.cs.groupten.stumblr.data.Route;
 import uk.ac.aber.cs.groupten.stumblr.data.Waypoint;
+import uk.ac.aber.cs.groupten.stumblr.system.AndroidLocation;
 
-public class CreateWaypoint extends ActionBarActivity {
+public class CreateWaypoint extends AbstractActivity {
 
     protected Context context;
     protected Criteria criteria;
-    protected PendingIntent singleUpatePI;
+    protected PendingIntent singleUpdatePI;
     protected LocationListener locationListener;
     protected LocationManager locationManager;
     protected static String SINGLE_LOCATION_UPDATE_ACTION;
 
-    public CreateWaypoint() {
-
-    }
+    public static final int CAMERA_REQ_CODE = 1337;
 
     /**
      * Loads the activity on creation (using a bundle if one is present)
@@ -43,101 +45,16 @@ public class CreateWaypoint extends ActionBarActivity {
         if (savedInstanceState == null) {
             // Do stuff
         }
+
+        getImage();
+        getLocation();
     }
-
-    /**
-     * Create a new Waypoint based on user input.
-     */
-    public  void createWaypoint(String title, String shortDesc, int index, Context context){
-        Waypoint wp = new Waypoint(title, shortDesc, index);
-
-            this.context = context;
-            locationManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
-            // Coarse accuracy is specified here to get the fastest possible result.
-            // The calling Activity will likely (or have already) request ongoing
-            // updates using the Fine location provider.
-            criteria = new Criteria();
-            criteria.setAccuracy(Criteria.ACCURACY_LOW);
-
-            // Construct the Pending Intent that will be broadcast by the oneshot
-            // location update.
-            Intent updateIntent = new Intent(SINGLE_LOCATION_UPDATE_ACTION);
-            singleUpatePI = PendingIntent.getBroadcast(context, 0, updateIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        wp.addCoordinate(getLastBestLocation(0,0));
-        }
-
-
-    /**
-     * This {@link BroadcastReceiver} listens for a single location
-     * update before unregistering itself.
-     * The oneshot location update is returned via the {@link LocationListener}
-     * specified in {@link setChangedLocationListener}.
-     */
-    protected BroadcastReceiver singleUpdateReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            context.unregisterReceiver(singleUpdateReceiver);
-
-            String key = LocationManager.KEY_LOCATION_CHANGED;
-            Location location = (Location)intent.getExtras().get(key);
-
-            if (locationListener != null && location != null)
-                locationListener.onLocationChanged(location);
-
-            locationManager.removeUpdates(singleUpatePI);
-        }
-    };
-
-  /* REFERENCE - GingerbreadLastLocation
-   * https://code.google.com/p/android-protips-location/source/browse/trunk/src/com/radioactiveyak/location_best_practices/utils/GingerbreadLastLocationFinder.java
-   */
-  @TargetApi(Build.VERSION_CODES.GINGERBREAD)
-  public Location getLastBestLocation(int minDistance, long minTime) {
-      Location bestResult = null;
-      float bestAccuracy = Float.MAX_VALUE;
-      long bestTime = Long.MIN_VALUE;
-
-      // Iterate through all the providers on the system, keeping
-      // note of the most accurate result within the acceptable time limit.
-      // If no result is found within maxTime, return the newest Location.
-      List<String> matchingProviders = locationManager.getAllProviders();
-      for (String provider: matchingProviders) {
-          Location location = locationManager.getLastKnownLocation(provider);
-          if (location != null) {
-              float accuracy = location.getAccuracy();
-              long time = location.getTime();
-
-              if ((time > minTime && accuracy < bestAccuracy)) {
-                  bestResult = location;
-                  bestAccuracy = accuracy;
-                  bestTime = time;
-              }
-              else if (time < minTime && bestAccuracy == Float.MAX_VALUE && time > bestTime) {
-                  bestResult = location;
-                  bestTime = time;
-              }
-          }
-      }
-      // If the best result is beyond the allowed time limit, or the accuracy of the
-      // best result is wider than the acceptable maximum distance, request a single update.
-      // This check simply implements the same conditions we set when requesting regular
-      // location updates every [minTime] and [minDistance].
-      if (locationListener != null && (bestTime < minTime || bestAccuracy > minDistance)) {
-          IntentFilter locIntentFilter = new IntentFilter(SINGLE_LOCATION_UPDATE_ACTION);
-          context.registerReceiver(singleUpdateReceiver, locIntentFilter);
-          locationManager.requestSingleUpdate(criteria, singleUpatePI);
-      }
-
-      return bestResult;
-  }
-
-
 
     /**
      * Obtain a photo from user and add it to current Waypoint.
      */
-    public void getImage(Waypoint w){
-
+    public void getImage(){
+        startCamera();
     }
 
     /**
@@ -150,29 +67,41 @@ public class CreateWaypoint extends ActionBarActivity {
     /**
      * Obtain coordinates from Android system and add to current Waypoint.
      */
+    public void getLocation() {
+        AndroidLocation al = new AndroidLocation(this);
+        Location loc = al.getLastBestLocation(0, 0);
 
-    // Acquire a reference to the system Location Manager
-    //LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lat: "); sb.append(loc.getLatitude());
+        sb.append(" Lon: "); sb.append(loc.getLongitude());
+        Log.v(TAG, sb.toString());
+    }
 
-    // Define a listener that responds to location updates
-    //LocationListener locationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            // Called when a new location is found by the network location provider.
-       //     makeUseOfNewLocation(location);
+    /*
+     * Camera stuff
+     */
+    public void startCamera() {
+        startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), CAMERA_REQ_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQ_CODE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap b = (Bitmap) extras.get("data");
+
+            if (b != null) {
+                // Log a message containing dimensions of image
+                StringBuilder sb = new StringBuilder();
+                sb.append("Image captured. Height: "); sb.append(b.getHeight());
+                sb.append(" Width: "); sb.append(b.getWidth());
+                Log.v(TAG, sb.toString());
+            }
         }
-
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
+    }
+};
 
 
 
