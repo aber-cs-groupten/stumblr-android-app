da8fd49efb35be075a45c379b7702bbf97d7a192
Charles Newey <charlie@assemblyco.de>
2014-01-25 15:24:47 +0000

    Added code to transfer Route between CreateRoute and WaypointList

--- Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/CreateRoute.java
+++ Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/CreateRoute.java
@@ -1,5 +1,6 @@
 package uk.ac.aber.cs.groupten.stumblr;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -7,21 +8,7 @@ import android.widget.TextView;
 import uk.ac.aber.cs.groupten.stumblr.data.Route;
 
 public class CreateRoute extends DataEntryActivity {
-
-     private String shortDesc, longDesc, title;
-
-    public CreateRoute() {
-        super();
-
-    }
-
-    /* New route method */
-    public void newRoute(){
-        shortDesc = ((TextView) findViewById(R.id.shortDescriptionBox)).getText().toString();
-        longDesc = ((TextView) findViewById(R.id.longDescriptionBox)).getText().toString();
-        title = ((TextView) findViewById(R.id.title)).getText().toString();
-        Route newRoute = new Route(title, shortDesc, longDesc);
-    }
+    private Route r;
 
     /**
      * Loads the activity on creation (using a bundle if one is present)
@@ -31,14 +18,34 @@ public class CreateRoute extends DataEntryActivity {
     public void stumblrOnCreate(Bundle savedInstanceState) {
         // Called by super().onCreate
         setContentView(R.layout.activity_create_route);
+
+        // Create new blank Route object
+        r = new Route();
     }
 
+    // TODO remove this method... shouldn't be here!
     public void end(View view) {
         finish();
     }
 
     /**
      * Start the WaypointList activity (list the current Route).
+     * Called when the "next" button is clicked in the UI.
      */
-    public void startWaypointListIntent(Route r) {};
+    public void startWaypointListIntent(View v) {
+        // Get text from fields in UI
+        String title = ((TextView) findViewById(R.id.routeTitle)).getText().toString();
+        String shortDesc = ((TextView) findViewById(R.id.shortDescriptionBox)).getText().toString();
+        String longDesc = ((TextView) findViewById(R.id.longDescriptionBox)).getText().toString();
+
+        // Set parameters of current Route object
+        r.setTitle(title);
+        r.setShortDesc(shortDesc);
+        r.setLongDesc(longDesc);
+
+        // Start the new Activity
+        Intent i = new Intent(getApplicationContext(), WaypointList.class);
+        i.putExtra("route", r);
+        startActivity(i);
+    }
 }
