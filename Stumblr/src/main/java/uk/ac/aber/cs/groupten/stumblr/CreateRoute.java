c53b04dfcb99da14ff8b23a62d6286a2031c9ee5
Daniel <dac46@aber.ac.uk>
2014-01-27 00:18:00 +0000

    refactored variable name

--- Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/CreateRoute.java
+++ Stumblr/src/main/java/uk/ac/aber/cs/groupten/stumblr/CreateRoute.java
@@ -14,7 +14,7 @@ import uk.ac.aber.cs.groupten.stumblr.data.Route;
 
 public class CreateRoute extends DataEntryActivity implements LocationListener {
     private LocationManager lm; // TODO this could be a local variable
-    private Route r;
+    private Route route;
 
     /**
      * Loads the activity on creation (using a bundle if one is present)
@@ -30,7 +30,7 @@ public class CreateRoute extends DataEntryActivity implements LocationListener {
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 10, this);
 
         // Create new blank Route object
-        r = new Route();
+        route = new Route();
     }
 
     /**
@@ -44,13 +44,13 @@ public class CreateRoute extends DataEntryActivity implements LocationListener {
         String longDesc = ((TextView) findViewById(R.id.longDescriptionBox)).getText().toString();
 
         // Set parameters of current Route object
-        r.setTitle(title);
-        r.setShortDesc(shortDesc);
-        r.setLongDesc(longDesc);
+        route.setTitle(title);
+        route.setShortDesc(shortDesc);
+        route.setLongDesc(longDesc);
 
         // Start the new Activity
         Intent i = new Intent(getApplicationContext(), WaypointList.class);
-        i.putExtra("route", r);
+        i.putExtra("route", route);
         startActivity(i);
     }
 
@@ -61,7 +61,7 @@ public class CreateRoute extends DataEntryActivity implements LocationListener {
     @Override
     public void onLocationChanged(Location loc) {
         Log.v(TAG, "Location updated.");
-        r.addCoordinate(loc);
+        route.addCoordinate(loc);
     }
 
     @Override
